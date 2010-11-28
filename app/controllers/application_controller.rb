9285469ba299b9912db3f395ebe4f93f10dfd2b9
Jeffrey Wilcke <stygeo@gmail.com>
2010-11-28 22:20:53 +0100

    Set current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :set_current_user_for_model
   
   protect_from_forgery
   after_filter :set_content_type
@@ -12,6 +12,10 @@ class ApplicationController < ActionController::Base
 
   def enki_config
     @@enki_config = Enki::Config.default
+  end
+  
+  def set_current_user_for_model
+    User.current = current_user
   end
   
   helper_method :enki_config
