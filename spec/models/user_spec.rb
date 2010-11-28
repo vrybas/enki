12a477b3f227ad5067aed4eb44384b2092f1d266
Jeffrey Wilcke <stygeo@gmail.com>
2010-11-28 22:23:44 +0100

    Added name spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it(:user) { Factory(:user, :firstname => "Miss", :lastname => "Panda")}
+  
+  it "should append first and lastname" do
+    user.should respond_to(:name)
+    user.name.should eq "Miss Panda"
+  end
 end
