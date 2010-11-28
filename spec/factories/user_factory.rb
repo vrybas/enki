ab2c022ba3f5c27d0495ab2d99980ed00db2b8f2
Jeffrey Wilcke <stygeo@gmail.com>
2010-11-28 22:22:36 +0100

    Added user factory

---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,8 @@
+Factory.define(:user) do |u|
+  u.username "agilepanda"
+  u.firstname "Agile"
+  u.lastname "Panda"
+  u.email "awesomepanda@agile-pandas.com"
+  u.password "123456"
+  u.password_confirmation "123456"
+end
